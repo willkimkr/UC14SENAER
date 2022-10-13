db01b07d02e2d9a89fb3afdfe30fce83d296dd2e
willkimkr <willkimkr@outlook.com>
2022-10-13 20:19:22 -0300

    ATIVIDADES

---
+++ ChapterBET6/ChapterBET6/Contexts/SqlContext.cs
@@ -0,0 +1,27 @@
+﻿using ChapterBET6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChapterBET6.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(){}
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options){}
+
+
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = LAPTOP-Q941SDLQ\\SQLEXPRESS; initial catalog = Chapter; Integrated Security = true");
+            }
+        }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro>? Livros { get; set; }
+    }
+}
+
+
